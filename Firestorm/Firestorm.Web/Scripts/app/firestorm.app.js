2b2bb881fe679afd8b11ba7274eec00337e1ac7b
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2015-11-09 22:56:24 -0200

    Configure Angular to Firestorm

---
+++ Firestorm/Firestorm.Web/Scripts/app/firestorm.app.js
@@ -0,0 +1,55 @@
+﻿require.config({
+    baseUrl: "/Scripts",
+    paths: {
+        "jquery": "Vendor/jquery/jquery",
+        "underscore": "Vendor/underscore/underscore",
+        "angular": "Vendor/angular/angular",
+        "ngResource": "Vendor/angular-resource/angular-resource",
+        "ngSanitize": "Vendor/angular-sanitize/angular-sanitize",
+        "bootstrap": "Vendor/bootstrap/bootstrap",
+        "material": "vendor/bootstrap-material-design/material",
+        "ripples": "vendor/bootstrap-material-design/ripples",
+    },
+    shim: {
+        underscore: {
+            exports: "_",
+        },
+        jquery: {
+            exports: "$"
+        },
+        angular: {
+            deps: ['jquery'],
+            exports: "angular"
+        },
+        bootstrap: {
+            deps: [
+                "jquery"
+            ]
+        },
+        material: {
+            deps: [
+                "ripples"
+            ]
+        },
+        ripples: {
+            deps: [
+                "jquery"
+            ]
+        },
+        ngResource: {
+            deps: [
+                "angular"
+            ]
+        },
+        ngSanitize: {
+            deps: [
+                "angular"
+            ]
+        }
+
+    }
+});
+
+require(['jquery', 'underscore', 'angular', 'material'], function ($, _, angular) {
+    $.material.init();
+});
---
+++ Firestorm/Firestorm.Web/Scripts/app/firestorm.main.js
@@ -0,0 +1,9 @@
+﻿define([
+    'angular',
+    'underscore',
+    'ngResource',
+    'ngSanitize',
+], function (angular, _, ngResource, ngSanitize) {
+    var app = angular.module('firestorm_app', ['ngResource', 'ngSanitize']);
+    return app;
+});
